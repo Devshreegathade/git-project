18387841417afd4ad49a7e86b7e68aeb7a2bb2c8
DEVSHREE  SUDHIR GATHADE <gdevshree01@gmail.com>
2026-04-27 11:23:24 +0530

    Add student details input and display functionality

---
+++ main.py
@@ -0,0 +1,12 @@
+# Taking input from user
+name = input("Devshree ")
+age = input("20")
+course = input("TE")
+college = input("ISBM")
+
+# Printing the details
+print("\n--- Student Details ---")
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+print("College:", college)
